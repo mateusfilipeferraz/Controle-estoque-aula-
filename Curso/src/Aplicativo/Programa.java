05c63e932c9b23bdc5e4e5d0dfa5cdea04fedd0c
Mateus Ferraz <mateus.p.ferraz@gmail.com>
2022-12-12 01:04:47 -0300

    minhas melhora no codigo do professor

--- Curso/src/Aplicativo/Programa.java
+++ Curso/src/Aplicativo/Programa.java
@@ -37,25 +37,37 @@ public class Programa {
         System.out.println("");
 
         System.out.println("Dados do produto:" + produto + "\n");
-        
-// até aqui foi orientação do professor mas resolvir incrementar com uma estrutura de repetição 
-
-        System.out.print("Entre com a quantidade de produtos para add no estoque:");
-        int quantidade = sc.nextInt();
-        produto.addProduto(quantidade);
         System.out.println("");
-        System.out.println("Atualização " + produto);
-        System.out.println("");
-        
-        
-        System.out.print("Entre com a quantidade para retirar do estoque:");
-        int quanidade =sc.nextInt();
-        produto.removerPoduto(quantidade);
-        System.out.println("");
-        System.out.println("Atualização " + produto);
+// até aqui foi orientação do professor mas resolvir incrementar com uma estrutura de repetição .
+        int escolha = 1;//iniciarlizar a variavel com 1.
+        //usei o while para enquanto no for 0 o resultado da escolha o programa se repete 
+        while (escolha != 0) {
 
-        sc.close();
+            //perguntando oque o usuario quer fazer no programa.
+            System.out.println("Para adicior produto digite 2 e para remover digite 3, sair 0");
+            escolha = sc.nextInt();
+            
+            if (escolha == 2) {
+                System.out.println("Digite a quantidade que quer adicionar:");
+                int quantidade = sc.nextInt();
+                produto.addProduto(quantidade);
+              
+            } else if (escolha == 3) {
+                System.out.println("Digite a quantidade que quer remover:");
+                int quantidade = sc.nextInt();
+                produto.removerPoduto(quantidade);
+            }else{
+                /* para sair usei uma expreção de escolha vezes 0 para sempre o ultimo else da zero 
+               para não precisarlimpar a varialvel escolha que e a saida do programa 
+                */
+                escolha*=0;
+            }
+            System.out.println("");
+            System.out.println("Atualização " + produto);
 
+           
+        }
+         sc.close();
     }
 
 }

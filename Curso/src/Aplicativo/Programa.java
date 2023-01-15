dfc6d96148ebcf8346d61a19e7a11eedb78e96ce
Mateus Ferraz <mateus.p.ferraz@gmail.com>
2023-01-15 18:03:48 -0300

    melhorias no cdigo final

--- Curso/src/Aplicativo/Programa.java
+++ Curso/src/Aplicativo/Programa.java
@@ -21,53 +21,73 @@ public class Programa {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        Produto produto = new Produto();
-
         System.out.println("Insira os dados do produto: ");
 
         System.out.print("Nome:");
-        produto.nome = sc.nextLine();
+        String nome = sc.nextLine();
 
         System.out.print("Preço:");
-        produto.preço = sc.nextDouble();
+        double preço = sc.nextDouble();
 
-        System.out.print("Quantidade no estoque:");
-        produto.quantidade = sc.nextInt();
-
-        System.out.println("");
-
-        System.out.println("Dados do produto:" + produto + "\n");
+        Produto produto = new Produto(nome, preço);
+        System.out.println("----------------------------------------------------------");
+        System.out.println("Dados do produto:" + produto);
+        System.out.println("----------------------------------------------------------");
         System.out.println("");
 // até aqui foi orientação do professor mas resolvir incrementar com uma estrutura de repetição .
-        int escolha = 1;//iniciarlizar a variavel com 1.
-        //usei o while para enquanto no for 0 o resultado da escolha o programa se repete 
-        while (escolha != 0) {
+        String escolha = "";
+        String controlador = "";
 
-            //perguntando oque o usuario quer fazer no programa.
-            System.out.println("Para adicior produto digite 2 e para remover digite 3, sair 0");
-            escolha = sc.nextInt();
-            
-            if (escolha == 2) {
-                System.out.println("Digite a quantidade que quer adicionar:");
-                int quantidade = sc.nextInt();
-                produto.addProduto(quantidade);
-              
-            } else if (escolha == 3) {
-                System.out.println("Digite a quantidade que quer remover:");
-                int quantidade = sc.nextInt();
-                produto.removerPoduto(quantidade);
-            }else{
-                /* para sair usei uma expreção de escolha vezes 0 para sempre o ultimo else da zero 
-               para não precisarlimpar a varialvel escolha que e a saida do programa 
-                */
-                escolha*=0;
+        System.out.println("Para adicionar produto digite 'add' e para remover 're' sair 's' ");
+        do {   //perguntando oque o usuario quer fazer no programa.
+
+            escolha = sc.nextLine();
+
+            if (null != escolha) {
+
+                switch (escolha) {
+                    case "add": {
+
+                        System.out.print("Digite a quantidade que quer adicionar:");
+                        int quantidade = sc.nextInt();
+                        produto.addProduto(quantidade);
+                        System.out.println("----------------------------------------------------------");
+                        System.out.println("Atualização " + produto);
+                        System.out.println("----------------------------------------------------------");
+                        System.out.println("Para adicionar produto digite 'add' e para remover 're' sair 's' ");
+                        break;
+                    }
+                    case "re": {
+
+                        System.out.print("Digite a quantidade que quer remover:");
+                        int quantidade = sc.nextInt();
+                        produto.removerPoduto(quantidade);
+                       System.out.println("----------------------------------------------------------");
+                        System.out.println("Atualização " + produto);
+                       System.out.println("----------------------------------------------------------");
+                        System.out.println("Para adicionar produto digite 'add' e para remover 're' sair 's' ");
+                        break;
+                    }
+                    case "s":
+                        controlador = escolha;
+                        System.out.println("Atualização " + produto);
+                        System.out.println("-------------------------");
+                        System.out.println("    PROGRAMA ENCERRADO.  ");
+                        System.out.println("-------------------------");
+                        break;
+
+                    default:
+                        break;
+                }
             }
-            System.out.println("");
-            System.out.println("Atualização " + produto);
 
-           
+        } while ("".equals(controlador));
+        {
+
         }
-         sc.close();
+
+        //perguntando oque o usuario quer fazer no programa.
+        sc.close();
     }
 
 }
--- Curso/src/Entidade/Produto.java
+++ Curso/src/Entidade/Produto.java
@@ -8,25 +8,61 @@ package Entidade;
  *
  * @author Mateus Ferraz
  */
-public  class Produto {
+public class Produto {
 
-    public String nome;
-    public double preço;
-    public int quantidade;
+    private String nome;
+    private double preço;
+    private double quantidade;
 
-    public  double totalEstoque() {
-        return this.preço *quantidade;
+    public Produto() {
+    }
+
+    public Produto(String nome, double preço, double quantidade) {
+        this.nome = nome;
+        this.preço = preço;
+        this.quantidade = quantidade;
+    }
+
+    public Produto(String nome, double preço) {
+        this.nome = nome;
+        this.preço = preço;
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreço() {
+        return preço;
+    }
+
+    public void setPreço(double preço) {
+        this.preço = preço;
+    }
+
+    public double getQuantidade() {
+        return quantidade;
+    }
+
+    public double totalEstoque() {
+        return this.preço * quantidade;
     }
 
     public void addProduto(int quantidade) {
         this.quantidade += quantidade;
     }
-    
-    public void removerPoduto (int quantidade){
-    this.quantidade-=quantidade;
+
+    public void removerPoduto(int quantidade) {
+        this.quantidade -= quantidade;
     }
-    public String toString () {
-        return nome + ", R$ "+String.format("%.2f",preço)+ ", " +quantidade+
-                " Unidade, Total: R$"+ String.format("%.2f", totalEstoque()) ;
-}
+
+    public String toString() {
+        return nome + ", R$ " + String.format("%.2f", preço) + ", " + quantidade
+                + " Unidade, Total: R$" + String.format("%.2f", totalEstoque());
+    }
 }
